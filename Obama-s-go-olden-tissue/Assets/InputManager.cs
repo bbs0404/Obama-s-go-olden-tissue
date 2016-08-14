cd831dcebd458ce4ce860ef994da83ba807765aa
HyunSik Jeong <HyunSik Jeong>
2016-08-15 05:43:03 +0900

    Small change in the InputManager

--- Obama-s-go-olden-tissue/Assets/InputManager.cs
+++ Obama-s-go-olden-tissue/Assets/InputManager.cs
@@ -145,149 +145,151 @@ public class InputManager : MonoBehaviour {
 
 
 	public void Update(){
-		GameObject temp = GameObject.Find("TitlePaper");
-		if(temp != null){
-			accuTime += Time.deltaTime;
-			while(accuTime > 0.6f){
-				accuTime -= 0.6f;
-			}
-			float tempScale = 1.2f - accuTime / 0.6f * 0.2f;
-			temp.transform.localScale = new Vector3(tempScale, tempScale, tempScale);
-		}
-
-		if(map != null){
-			int mapX = (int)((Input.mousePosition.x-20.0f) / (680f / map.GetLength(0)));
-			int mapY = (int)((720.0f - Input.mousePosition.x) / (680f / map.GetLength(0)));
-			PassPeople movingPeople = GameManager.Inst().getMovingPeople();
-			for(int i=0; i<mapSize; i++){
-				for(int j=0;j<mapSize;j++){
-					targetMapTiles[i,j].GetComponent<Image>().enabled = false; 
+		if(System.String.Compare(Application.loadedLevelName, "Title Scene", false) == 0){
+			GameObject temp = GameObject.Find("TitlePaper");
+			if(temp != null){
+				accuTime += Time.deltaTime;
+				while(accuTime > 0.6f){
+					accuTime -= 0.6f;
 				}
-			}
-			if(mapX >= 0 && mapX < mapSize && mapY >= 0 && mapY < mapSize
-				&& map[mapX,mapY] != null && movingPeople != null){
-				foreach(var item in movingPeople.PassLocation){
-					if((int)item.x + mapX >= 0 && (int)item.x + mapX < mapSize &&
-						(int)item.y + mapY >= 0 && (int)item.y + mapY < mapSize &&
-						map[(int)item.x + mapX,(int)item.y + mapY] != null)
-						targetMapTiles[(int)item.x + mapX,(int)item.y + mapY].GetComponent<Image>().enabled = true;
-				}
+				float tempScale = 1.2f - accuTime / 0.6f * 0.2f;
+				temp.transform.localScale = new Vector3(tempScale, tempScale, tempScale);
 			}
 		}
-		else if(System.String.Compare(Application.loadedLevelName, "Main Scene", false) == 0 &&  GameManager.Inst().getMap()!=null){
-			map = GameManager.Inst().getMap();
-			mapSize = map.GetLength(0);
-			targetMapTiles = new GameObject[mapSize, mapSize];
-			for(int i=0; i<mapSize; i++){
-				for(int j=0;j<mapSize;j++){
-					targetMapTiles[i,j] = Instantiate(Resources.Load("TargetTile")) as GameObject;
-					targetMapTiles[i,j].transform.SetParent((GameObject.Find("Canvas")).transform, false);
-					targetMapTiles[i,j].transform.position = new Vector2(20 + i * (680f / mapSize) + (680f / mapSize) / 2, 720 - (20 + j * (680f / mapSize) + (680f / mapSize) / 2));
-					targetMapTiles[i,j].transform.localScale = new Vector2(6.80f / mapSize, 6.80f / mapSize);
-					targetMapTiles[i,j].GetComponent<Image>().enabled = false;
-				}
-			}
-		}
-		
-		
-		// When Failed
-		if(failedFlag){
-			accuTime += Time.deltaTime;
-			// While the first 0.5 sec, Failed Chocolate Background comes down.
-			if(accuTime < 0.5){
-				Image failedBG = GameObject.Find("FailedBackground").GetComponent<Image>();
-				if(failedState == 0){
-					failedBG.enabled = true;
-					failedBG.transform.SetAsLastSibling();
-					failedState = 1;
-				}
-				failedBG.transform.position = new Vector3(640, 1260 - accuTime/0.5f * 1080f, 0);
-			}
-			// While the next 0.5 sec, Failed Tr__p appears with increasing its alpha value. Also, Failed text comes down.
-			else if(accuTime < 1.0){
-				Image failedTr__p = GameObject.Find("FailedTr__p").GetComponent<Image>();
-				if(failedState == 1){
-					GameObject.Find("FailedBackground").GetComponent<Image>().transform.position = new Vector3(640, 180, 0);
-					failedTr__p.enabled = true;
-					failedTr__p.transform.SetAsLastSibling();
-					failedTr__p.transform.position = new Vector3(640, 360, 0);
-					failedState = 2;
-				}
-				Color colorTemp = failedTr__p.color;
-            	colorTemp.a = ((accuTime-0.5f)*2.1f > 1.0f) ? 1.0f : (accuTime-0.5f)*2.1f;
-            	failedTr__p.color = colorTemp;
-				// Failed Text comes after Tr__p's alpha value is 0.5.
-				if(accuTime>=0.75){
-					Image failedText = GameObject.Find("FailedText").GetComponent<Image>();
-					if(failedState == 2){
-						failedText.enabled = true;
-						failedText.transform.SetAsLastSibling();
-						failedState = 3;
+		else if(System.String.Compare(Application.loadedLevelName, "Main Scene", false) == 0){
+			if(map != null){
+				int mapX = (int)((Input.mousePosition.x-20.0f) / (680f / map.GetLength(0)));
+				int mapY = (int)((720.0f - Input.mousePosition.x) / (680f / map.GetLength(0)));
+				PassPeople movingPeople = GameManager.Inst().getMovingPeople();
+				for(int i=0; i<mapSize; i++){
+					for(int j=0;j<mapSize;j++){
+						targetMapTiles[i,j].GetComponent<Image>().enabled = false; 
 					}
-					failedText.transform.position = new Vector3(640, 1080 - (accuTime-0.75f)/0.25f * 600f, 0);
+				}
+				if(mapX >= 0 && mapX < mapSize && mapY >= 0 && mapY < mapSize
+					&& map[mapX,mapY] != null && movingPeople != null){
+					foreach(var item in movingPeople.PassLocation){
+						if((int)item.x + mapX >= 0 && (int)item.x + mapX < mapSize &&
+							(int)item.y + mapY >= 0 && (int)item.y + mapY < mapSize &&
+							map[(int)item.x + mapX,(int)item.y + mapY] != null)
+							targetMapTiles[(int)item.x + mapX,(int)item.y + mapY].GetComponent<Image>().enabled = true;
+					}
 				}
 			}
-			// After 1 sec, Button appears.
-			else if(failedState == 3){
-				GameObject.Find("FailedText").GetComponent<Image>().transform.position = new Vector3(640, 480, 0);
-				GameObject failedRestart = GameObject.Find("FailedRestartButton");
-				Image failedRestartImage = failedRestart.GetComponent<Image>();
-				failedRestartImage.enabled = true;
-				failedRestartImage.transform.SetAsLastSibling();
-				failedRestartImage.transform.position = new Vector3(640, 120, 0);
-				Button failedRestartButton = failedRestart.GetComponent<Button>();
-				failedRestartButton.interactable = true;
-				failedState = 4;
-			}
-		}
-		// When Success
-		if(successFlag){
-			// Gol-den tissue spins constantly. It's awesome. 
-			GameObject tissue = GameObject.Find("tissue");
-			tissue.transform.RotateAround(tissue.transform.position, Vector3.up, 50.0f * Time.deltaTime);
-			accuTime += Time.deltaTime;
-			// While the first 0.2sec, Success Background appears by increasing its alpha value.
-			if(accuTime<0.2f){
-				Image successBackground = GameObject.Find("SuccessBackground").GetComponent<Image>();
-				if(successState == 0){
-					successBackground.enabled = true;
-					successBackground.transform.SetAsLastSibling();
-					successState = 1;
+			else if(GameManager.Inst().getMap()!=null){
+				map = GameManager.Inst().getMap();
+				mapSize = map.GetLength(0);
+				targetMapTiles = new GameObject[mapSize, mapSize];
+				for(int i=0; i<mapSize; i++){
+					for(int j=0;j<mapSize;j++){
+						targetMapTiles[i,j] = Instantiate(Resources.Load("TargetTile")) as GameObject;
+						targetMapTiles[i,j].transform.SetParent((GameObject.Find("Canvas")).transform, false);
+						targetMapTiles[i,j].transform.position = new Vector2(20 + i * (680f / mapSize) + (680f / mapSize) / 2, 720 - (20 + j * (680f / mapSize) + (680f / mapSize) / 2));
+						targetMapTiles[i,j].transform.localScale = new Vector2(6.80f / mapSize, 6.80f / mapSize);
+						targetMapTiles[i,j].GetComponent<Image>().enabled = false;
+					}
 				}
-				Color colorTemp = successBackground.color;
-            	colorTemp.a = (accuTime * 6.0f > 1.0f) ? 1.0f : (accuTime * 6.0f);
-            	successBackground.color = colorTemp;
 			}
-			// While the next 0.3 sec, Removes the original canvas and let the success canvas appears.
-			// And then, Ribbon starts to come down.
-			else if(accuTime<0.5f){
-				Image successRibbon = GameObject.Find("SuccessRibbon").GetComponent<Image>();
-				if(successState == 1){
-					GameObject.Find("Canvas").SetActive(false);
-					Canvas canvasSuc = GameObject.Find("CanvasSuc").GetComponent<Canvas>();
-					canvasSuc.enabled = true;
-					successState = 2;
+
+			// When Failed
+			if(failedFlag){
+				accuTime += Time.deltaTime;
+				// While the first 0.5 sec, Failed Chocolate Background comes down.
+				if(accuTime < 0.5){
+					Image failedBG = GameObject.Find("FailedBackground").GetComponent<Image>();
+					if(failedState == 0){
+						failedBG.enabled = true;
+						failedBG.transform.SetAsLastSibling();
+						failedState = 1;
+					}
+					failedBG.transform.position = new Vector3(640, 1260 - accuTime/0.5f * 1080f, 0);
 				}
-				// Tissue also comes down
-				if(accuTime<0.4f)
-					tissue.transform.position = new Vector3(0, 10 - (accuTime-0.2f)/0.2f * 12, 100);
-				else
-					tissue.transform.position = new Vector3(0, -2, 100);
-				successRibbon.transform.position = new Vector3(640, 1080 - (accuTime-0.2f)/0.3f * 720, 100);
+				// While the next 0.5 sec, Failed Tr__p appears with increasing its alpha value. Also, Failed text comes down.
+				else if(accuTime < 1.0){
+					Image failedTr__p = GameObject.Find("FailedTr__p").GetComponent<Image>();
+					if(failedState == 1){
+						GameObject.Find("FailedBackground").GetComponent<Image>().transform.position = new Vector3(640, 180, 0);
+						failedTr__p.enabled = true;
+						failedTr__p.transform.SetAsLastSibling();
+						failedTr__p.transform.position = new Vector3(640, 360, 0);
+						failedState = 2;
+					}
+					Color colorTemp = failedTr__p.color;
+					colorTemp.a = ((accuTime-0.5f)*2.1f > 1.0f) ? 1.0f : (accuTime-0.5f)*2.1f;
+					failedTr__p.color = colorTemp;
+					// Failed Text comes after Tr__p's alpha value is 0.5.
+					if(accuTime>=0.75){
+						Image failedText = GameObject.Find("FailedText").GetComponent<Image>();
+						if(failedState == 2){
+							failedText.enabled = true;
+							failedText.transform.SetAsLastSibling();
+							failedState = 3;
+						}
+						failedText.transform.position = new Vector3(640, 1080 - (accuTime-0.75f)/0.25f * 600f, 0);
+					}
+				}
+				// After 1 sec, Button appears.
+				else if(failedState == 3){
+					GameObject.Find("FailedText").GetComponent<Image>().transform.position = new Vector3(640, 480, 0);
+					GameObject failedRestart = GameObject.Find("FailedRestartButton");
+					Image failedRestartImage = failedRestart.GetComponent<Image>();
+					failedRestartImage.enabled = true;
+					failedRestartImage.transform.SetAsLastSibling();
+					failedRestartImage.transform.position = new Vector3(640, 120, 0);
+					Button failedRestartButton = failedRestart.GetComponent<Button>();
+					failedRestartButton.interactable = true;
+					failedState = 4;
+				}
 			}
-			// After 0.5 sec, Button appears.
-			else if(successState == 2){
-				GameObject.Find("SuccessRibbon").GetComponent<Image>().transform.position = new Vector3(640, 360, 100);
-				GameObject successRestart = GameObject.Find("SuccessRestartButton");
-				Image successRestartImage = successRestart.GetComponent<Image>();
-				successRestartImage.enabled = true;
-				Button successRestartButton = successRestart.GetComponent<Button>();
-				successRestartButton.interactable = true;
-				GameObject successNext = GameObject.Find("SuccessNextButton");
-				Image successNextImage = successNext.GetComponent<Image>();
-				successNextImage.enabled = true;
-				Button successNextButton = successNext.GetComponent<Button>();
-				successNextButton.interactable = true;
+			// When Success
+			if(successFlag){
+				// Gol-den tissue spins constantly. It's awesome. 
+				GameObject tissue = GameObject.Find("tissue");
+				tissue.transform.RotateAround(tissue.transform.position, Vector3.up, 50.0f * Time.deltaTime);
+				accuTime += Time.deltaTime;
+				// While the first 0.2sec, Success Background appears by increasing its alpha value.
+				if(accuTime<0.2f){
+					Image successBackground = GameObject.Find("SuccessBackground").GetComponent<Image>();
+					if(successState == 0){
+						successBackground.enabled = true;
+						successBackground.transform.SetAsLastSibling();
+						successState = 1;
+					}
+					Color colorTemp = successBackground.color;
+					colorTemp.a = (accuTime * 6.0f > 1.0f) ? 1.0f : (accuTime * 6.0f);
+					successBackground.color = colorTemp;
+				}
+				// While the next 0.3 sec, Removes the original canvas and let the success canvas appears.
+				// And then, Ribbon starts to come down.
+				else if(accuTime<0.5f){
+					Image successRibbon = GameObject.Find("SuccessRibbon").GetComponent<Image>();
+					if(successState == 1){
+						GameObject.Find("Canvas").SetActive(false);
+						Canvas canvasSuc = GameObject.Find("CanvasSuc").GetComponent<Canvas>();
+						canvasSuc.enabled = true;
+						successState = 2;
+					}
+					// Tissue also comes down
+					if(accuTime<0.4f)
+						tissue.transform.position = new Vector3(0, 10 - (accuTime-0.2f)/0.2f * 12, 100);
+					else
+						tissue.transform.position = new Vector3(0, -2, 100);
+					successRibbon.transform.position = new Vector3(640, 1080 - (accuTime-0.2f)/0.3f * 720, 100);
+				}
+				// After 0.5 sec, Button appears.
+				else if(successState == 2){
+					GameObject.Find("SuccessRibbon").GetComponent<Image>().transform.position = new Vector3(640, 360, 100);
+					GameObject successRestart = GameObject.Find("SuccessRestartButton");
+					Image successRestartImage = successRestart.GetComponent<Image>();
+					successRestartImage.enabled = true;
+					Button successRestartButton = successRestart.GetComponent<Button>();
+					successRestartButton.interactable = true;
+					GameObject successNext = GameObject.Find("SuccessNextButton");
+					Image successNextImage = successNext.GetComponent<Image>();
+					successNextImage.enabled = true;
+					Button successNextButton = successNext.GetComponent<Button>();
+					successNextButton.interactable = true;
+				}
 			}
 		}
 	}
